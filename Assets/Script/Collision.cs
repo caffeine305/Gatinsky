22082801c8af6b1b89cec86dd87c035d94c01b13
Ricardo Lázaro Zermeño <caffeine305gmail.com>
2017-02-07 07:51:14 -0600

    Initial files. Character can move and jump. Jump needs to be polished

---
+++ Assets/Script/Collision.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collision : MonoBehaviour {
+
+    public static bool onGround;
+
+    void onCollisionEnter(Collision2D col)
+        {
+        if (col.gameObject.name == "Ground")
+            onGround = true;
+        else
+            onGround = false;
+        }
+
+    }
---
+++ Assets/Script/Movement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+    public float moveSpeed = 10.0f;
+    public float gravity = 9.8f;
+    public bool onGround;
+    public bool isJumping;
+
+    void Start(){
+        onGround = true;
+        isJumping = false;
+    }
+
+    void OnCollisionStay2D(Collision2D col)
+    {
+        onGround = true;
+        isJumping = false;
+    }
+
+    void FixedUpdate() {
+
+
+        float vel = Input.GetAxis("Horizontal") * moveSpeed; 
+        transform.Translate(Vector2.right * vel * Time.deltaTime);
+
+        if (Input.GetButton("Fire1")){
+
+            if ((onGround == true) && (isJumping == false))
+            {
+                transform.Translate(Vector2.up * gravity * Time.deltaTime);
+                isJumping = true;
+                onGround = false;
+            }
+        }
+    }
+
+
+}
